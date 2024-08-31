ef7e7c0ac575520a3ab876bb78d3fea527309d42
Rayko Azcue Pérez <razcue@yandex.com>
2024-08-31 12:39:32 -0400

    Modules federation setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
+import { URL } from 'url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    federation({
+      name: 'mf.razcue.github.io',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './HelloWorld': './src/components/HelloWorld.vue',
+      },
+      shared: ['vue', 'vuetify'],
+    }),
+  ],
   base: '',
+  resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+            `@import "~@/styles/variables.scss";`,
+      },
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
 })
