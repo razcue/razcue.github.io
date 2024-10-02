a7a270704b50398a72403f3dfcb064994665e62b
razcue <razcue@users.noreply.github.com>
2024-10-02 12:01:59 +0000

    deploy: 436d5bfe43eabec11ab099f79db6f58bb23efc56

---
+++ storybook/assets/HelloWorld.stories-BGYIf0oa.js
@@ -0,0 +1,31 @@
+import t from"./__federation_expose_HelloWorld-BCc8HJCz.js";const n={title:"Example/HelloWorld",component:t,argTypes:{msg:{control:"text"}}},o=a=>({components:{HelloWorld:t},setup(){return{args:a}},template:'<HelloWorld v-bind="args" />'}),e=o.bind({});e.args={msg:"Hello from a custom message!"};const s=o.bind({});s.args={msg:"Check out this styled message!"};const r=o.bind({});r.args={msg:"Click the button to increase the count."};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`args => ({
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: '<HelloWorld v-bind="args" />'
+})`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`args => ({
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: '<HelloWorld v-bind="args" />'
+})`,...s.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`args => ({
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: '<HelloWorld v-bind="args" />'
+})`,...r.parameters?.docs?.source}}};const m=["CustomMessage","CSSStyledMessage","CounterTest"];export{s as CSSStyledMessage,r as CounterTest,e as CustomMessage,m as __namedExportsOrder,n as default};
